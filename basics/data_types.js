aa911f2becfee32d19b7f5bcc30d7e6a29adcbe8
VishalGupta707 <vishalgupta70777@gmail.com>
2025-09-13 10:43:08 +0000

    data types

---
+++ basics/data_types.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat All JS code as newer version
+
+// alert(3+3);   This is used is browser only 
+
+console.log(3+3);
+
+//Types
+
+//  1- Primitive Data Type
+// Number -> Range till 2^53
+// bigInt -> For large value
+// boolean -> True || False
+// string -> "vishal"
+// null -> standalone value
+// undefined
+// symbol -> unique
+
+//2-Object
+
+console.log(typeof null);
+//return object as type
+
+
+console.log(typeof undefined);
+//return undefined as type
