cf86588c5e15738162dcec96dd4ae57db81b0e02
VishalGupta707 <vishalgupta70777@gmail.com>
2025-09-21 08:55:38 +0000

    First

---
+++ 05-iteration/one.js
@@ -0,0 +1,19 @@
+for (let index = 0; index < 10; index++) {
+    const element = 10
+  //  console.log(element);
+}
+
+
+for (let i = 0; i < 10; i++) {
+    if(i==5){
+        continue
+        // means continue for this time and do next operation
+    }
+   // console.log(i);
+    
+}
+
+const array=[1,2,3,4,5]
+array.forEach(element => {
+    console.log(element);
+});
